888a39449b55e6c5ab416bf82142218b91935a50
John Paul Pacatang <59640442+JohnPaulPacatang@users.noreply.github.com>
2023-09-20 06:06:23 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+function scrollToElement(elementSelector, instance = 0) {
+  const elements = document.querySelectorAll(elementSelector);
+  if (elements.length > instance) {
+    elements[instance].scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+const link4 = document.getElementById("link4");
+const hireMeButton = document.getElementById("hireMeButton");
+
+
+link1.addEventListener("click", () => {
+  scrollToElement(".Services");
+});
+
+link2.addEventListener("click", () => {
+  scrollToElement(".Skills");
+});
+
+link3.addEventListener("click", () => {
+  scrollToElement(".Project");
+});
+
+link4.addEventListener("click", () => {
+  scrollToElement(".column");
+});
+
+hireMeButton.addEventListener("click", () => {
+  scrollToElement(".column");
+});
+
+const toggleButton = document.getElementById("navbar-toggle");
+const closeButton = document.getElementById("close-button");
+const navLinks = document.querySelector(".nav-links");
+
+toggleButton.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+closeButton.addEventListener("click", () => {
+  navLinks.classList.remove("active");
+});
+
+
+
+
+
